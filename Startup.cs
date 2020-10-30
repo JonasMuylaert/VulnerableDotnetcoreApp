73db6411846d5fb3a5a686be6bad534ceeb4dfd7
JonasMuylaert <jonas.muylaert@live.com>
2020-10-30 16:00:47 +0100

    test

--- Startup.cs
+++ Startup.cs
@@ -47,8 +47,8 @@ namespace VulnerableApplication
         {
 
             app.UseDeveloperExceptionPage();
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+         //e this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts(); 
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
